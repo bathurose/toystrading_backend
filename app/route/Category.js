2ded248d330610a2bbcf291e3ccac5dba9eea5cd
bathurose <56596266+bathurose@users.noreply.github.com>
2020-12-14 13:54:12 +0700

    get categoy

--- app/route/Category.js
+++ app/route/Category.js
@@ -5,7 +5,7 @@
 const CategoryCtrl = require('../controllers/CategoryCtrl');
 module.exports = function (app) {
     app.post('/v1/auth/category', CategoryCtrl.create);
-    app.get('/v1/auth/category', CategoryCtrl.getAll);
+    app.get('/v1/category', CategoryCtrl.getAll);
     app.get('/v1/auth/category/getCB', CategoryCtrl.getAllCB);
     app.put('/v1/auth/category/:id', CategoryCtrl.update);
     app.delete('/v1/auth/category/:id', CategoryCtrl.delete);

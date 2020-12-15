6b4f0132b2a12a82ffe49c0831ffcc94fca5cd88
bathurose <56596266+bathurose@users.noreply.github.com>
2020-12-15 22:53:40 +0700

    Update Tag.js

--- app/route/Tag.js
+++ app/route/Tag.js
@@ -6,7 +6,7 @@ const TagCtrl = require('../controllers/TagCtrl');
 const Tag = require('../models/Tag');
 module.exports = function (app) {
     app.post('/v1/auth/tag', TagCtrl.create);
-    app.get('/v1/auth/tag', TagCtrl.getAll);
+    app.get('/v1/tag', TagCtrl.getAll);
     app.put('/v1/auth/tag/:id', TagCtrl.update);
     app.delete('/v1/auth/tag/:id', TagCtrl.delete);
 };

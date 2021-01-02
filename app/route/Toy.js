d6fd81aaab70cce287ad56bfe16e837f0df61759
bathurose <56596266+bathurose@users.noreply.github.com>
2021-01-02 22:37:21 +0700

    Update Toy.js

--- app/route/Toy.js
+++ app/route/Toy.js
@@ -8,6 +8,7 @@ module.exports = function (app) {
     app.post('/v1/auth/toy', ToyCtrl.create);
     app.get('/v1/toy', ToyCtrl.getAll);
     app.get('/v1/get_new_toy', ToyCtrl.get_new_toy);
+    app.get('/v1/auth/get_toy_byid', ToyCtrl.get_toy_byid);
     app.put('/v1/auth/toy/:id', ToyCtrl.update);
     app.get('/v1/toy/:id', ToyCtrl.getOne);
     app.get('/v1/toy_by_user/:id', ToyCtrl.getToyByUser);
